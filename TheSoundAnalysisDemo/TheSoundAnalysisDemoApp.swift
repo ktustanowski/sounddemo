cb640343868787fb4a01b904bb1b6c94ca3487e8
Kamil Tustanowski <kamil.tustanowski@gmail.com>
2021-11-17 19:57:30 +0100

    Initial Commit

---
+++ TheSoundAnalysisDemo/TheSoundAnalysisDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheSoundAnalysisDemoApp.swift
+//  TheSoundAnalysisDemo
+//
+//  Created by Semerkchet on 17/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct TheSoundAnalysisDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
