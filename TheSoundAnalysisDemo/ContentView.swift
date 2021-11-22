e23676d679854182b42993701281b5566aa82805
Kamil Tustanowski <kamil.tustanowski@gmail.com>
2021-11-22 18:22:13 +0100

    Refactor

--- TheSoundAnalysisDemo/ContentView.swift
+++ TheSoundAnalysisDemo/ContentView.swift
@@ -2,20 +2,145 @@
 //  ContentView.swift
 //  TheSoundAnalysisDemo
 //
-//  Created by Semerkchet on 17/11/2021.
+//  Created by Kamil Tustanowski on 17/11/2021.
 //
 
 import SwiftUI
+import Speech
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+final class SpeechAnalyzer: NSObject, ObservableObject, SFSpeechRecognizerDelegate {
+    private let audioEngine = AVAudioEngine()
+    private var inputNode: AVAudioInputNode?
+    private var speechRecognizer: SFSpeechRecognizer?
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    
+    @Published var recognizedText: String?
+    @Published var isProcessing: Bool = false
+
+    func start() {
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        } catch {
+            print("Couldn't configure the audio session properly")
+        }
+        
+        inputNode = audioEngine.inputNode
+        
+        speechRecognizer = SFSpeechRecognizer()
+        print("Supports on device recognition: \(speechRecognizer?.supportsOnDeviceRecognition == true ? "✅" : "🔴")")
+
+        // Force specified locale
+        // self.speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "pl_PL"))
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        // Disable partial results
+        // recognitionRequest?.shouldReportPartialResults = false
+        
+        // Enable on-device recognition
+        // recognitionRequest?.requiresOnDeviceRecognition = true
+
+        guard let speechRecognizer = speechRecognizer,
+              speechRecognizer.isAvailable,
+              let recognitionRequest = recognitionRequest,
+              let inputNode = inputNode
+        else {
+            assertionFailure("Unable to start the speech recognition!")
+            return
+        }
+        
+        speechRecognizer.delegate = self
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            recognitionRequest.append(buffer)
+        }
+
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { [weak self] result, error in
+            self?.recognizedText = result?.bestTranscription.formattedString
+            
+            guard error != nil || result?.isFinal == true else { return }
+            self?.stop()
+        }
+
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+            isProcessing = true
+        } catch {
+            print("Coudn't start audio engine!")
+            stop()
+        }
+    }
+    
+    func stop() {
+        recognitionTask?.cancel()
+        
+        self.audioEngine.stop()
+        inputNode?.removeTap(onBus: 0)
+        
+        isProcessing = false
+        
+        recognitionRequest = nil
+        recognitionTask = nil
+        speechRecognizer = nil
+        inputNode = nil
+    }
+    
+    public func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            print("✅ Available")
+        } else {
+            print("🔴 Unavailable")
+            recognizedText = "Text recognition unavailable. Sorry!"
+            stop()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct SpeechRecognitionView: View {
+    private enum Constans {
+        static let recognizeButtonSide: CGFloat = 100
+    }
+    
+    @ObservedObject private var speechAnalyzer = SpeechAnalyzer()
+    var body: some View {
+        VStack {
+            Spacer()
+            Text(speechAnalyzer.recognizedText ?? "Tap to begin")
+                .padding()
+            
+            Button {
+                toggleSpeechRecognition()
+            } label: {
+                Image(systemName: speechAnalyzer.isProcessing ? "waveform.circle.fill" : "waveform.circle")
+                    .resizable()
+                    .frame(width: Constans.recognizeButtonSide,
+                           height: Constans.recognizeButtonSide,
+                           alignment: .center)
+                    .foregroundColor(speechAnalyzer.isProcessing ? .red : .gray)
+                    .aspectRatio(contentMode: .fit)
+            }
+            .padding()
+        }
+    }
+}
+
+private extension SpeechRecognitionView {
+    func toggleSpeechRecognition() {
+        if speechAnalyzer.isProcessing {
+            speechAnalyzer.stop()
+        } else {
+            speechAnalyzer.start()
+        }
+    }
+}
+
+struct SpeechRecognitionView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        SpeechRecognitionView()
     }
 }
